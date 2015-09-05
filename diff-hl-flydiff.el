f4b677b67064a3250e6ab4348de09987e2232a0a
PythonNut <PythonNut@users.noreply.github.com>
2015-09-05 07:08:21 -0700

    Use diff-hl-with-diff-switches for diff-hl-flydiff-mode

--- diff-hl-flydiff.el
+++ diff-hl-flydiff.el
@@ -122,8 +122,12 @@ This requires the external program `diff' to be in your `exec-path'."
                temporary-file-directory))
             (rev (diff-hl-flydiff-create-revision file
                    (diff-hl-flydiff/working-revision file))))
-      (diff-no-select rev (current-buffer) "-U 0 --strip-trailing-cr" 'noasync
-        (get-buffer-create " *diff-hl-diff*")))))
+      (diff-hl-with-diff-switches
+        (diff-no-select rev
+          (current-buffer)
+          "-U 0 --strip-trailing-cr"
+          'noasync
+          (get-buffer-create " *diff-hl-diff*"))))))
 
 (defun diff-hl-flydiff/update (old-fun &optional auto)
   (unless (and auto
