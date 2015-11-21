89e08d60c4e8d5d4ddd47e14c0dd769ba965a545
Dmitry Gutov <dgutov@yandex.ru>
2015-11-21 03:15:36 +0200

    diff-hl-revert-hunk: Kill the diff buffer afterwards
    Closes #57.

--- diff-hl.el
+++ diff-hl.el
@@ -417,7 +417,7 @@ in the source file, or the last line of the hunk above it."
               (with-current-buffer ,buffer
                 (save-buffer))
               (message "Hunk reverted"))))
-      (quit-windows-on diff-buffer))))
+      (quit-windows-on diff-buffer t))))
 
 (defun diff-hl-hunk-overlay-at (pos)
   (cl-loop for o in (overlays-in pos (1+ pos))
