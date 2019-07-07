374b1d4b34876219218da7a539e7301259b56958
Dmitry Gutov <dgutov@yandex.ru>
2019-07-08 01:43:12 +0300

    Bump the version

--- diff-hl.el
+++ diff-hl.el
@@ -5,7 +5,7 @@
 ;; Author:   Dmitry Gutov <dgutov@yandex.ru>
 ;; URL:      https://github.com/dgutov/diff-hl
 ;; Keywords: vc, diff
-;; Version:  1.8.6
+;; Version:  1.8.7
 ;; Package-Requires: ((cl-lib "0.2") (emacs "24.3"))
 
 ;; This file is part of GNU Emacs.
