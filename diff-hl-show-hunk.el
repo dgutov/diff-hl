40c89a7b0d2590b1e094128f3f1d6dc87bf14ce9
Dmitry Gutov <dgutov@yandex.ru>
2021-09-09 05:07:53 +0300

    Revert "Make diff-hl-show-hunk's key bindings more consistent with the base set"
    This reverts commit 983366cb40923df23dd26ab38f616a1791094653.

--- diff-hl-show-hunk.el
+++ diff-hl-show-hunk.el
@@ -212,8 +212,10 @@ Returns a list with the buffer and the line number of the clicked line."
 
 (defvar diff-hl-show-hunk--inline-popup-map
   (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "p") #'diff-hl-show-hunk-previous)
+    (define-key map (kbd "n") #'diff-hl-show-hunk-next)
     (define-key map (kbd "c") #'diff-hl-show-hunk-copy-original-text)
-    (define-key map (kbd "n") #'diff-hl-show-hunk-revert-hunk)
+    (define-key map (kbd "r") #'diff-hl-show-hunk-revert-hunk)
     (define-key map (kbd "[") #'diff-hl-show-hunk-previous)
     (define-key map (kbd "]") #'diff-hl-show-hunk-next)
     (define-key map (kbd "{") #'diff-hl-show-hunk-previous)
@@ -271,7 +273,7 @@ BUFFER is a buffer with the hunk."
          (if (and (boundp 'diff-hl-reference-revision) diff-hl-reference-revision)
              (concat "Diff with " diff-hl-reference-revision)
            "Diff with HEAD")
-         "(q)Quit  (\[)Previous  (\])Next  (n)Revert  (c)Copy original"
+         "(q)Quit  (p)Previous  (n)Next  (r)Revert  (c)Copy original"
          diff-hl-show-hunk--inline-popup-map
          #'diff-hl-show-hunk-hide
          point
