21b65664e656f140577cd66f0658b34500627d38
Dmitry Gutov <dgutov@yandex.ru>
2023-08-07 17:59:12 +0300

    Delete the conditional 'defadvice' since we require min Emacs 25+ already
    Resolves #203

--- diff-hl-dired.el
+++ diff-hl-dired.el
@@ -144,16 +144,6 @@ for DIR containing FILES. Call UPDATE-FUNCTION as entries are added."
       (vc-call-backend backend 'dir-status-files dir files update-function)
     (vc-call-backend backend 'dir-status-files dir files nil update-function)))
 
-(when (version< emacs-version "24.4.51.5")
-  ;; Work around http://debbugs.gnu.org/19386
-  (defadvice vc-git-dir-status-goto-stage (around
-                                           diff-hl-dired-skip-up-to-date
-                                           (stage files update-function)
-                                           activate)
-    (when (eq stage 'ls-files-up-to-date)
-      (setq stage 'diff-index))
-    ad-do-it))
-
 (defun diff-hl-dired-highlight-items (alist)
   "Highlight ALIST containing (FILE . TYPE) elements."
   (dolist (pair alist)
