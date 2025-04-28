6cc23ae06aee6cb96698c047d96eb9a09efd82c1
Dmitry Gutov <dmitry@gutov.dev>
2025-04-28 04:10:21 +0300

    Use skip-unless

--- test/diff-hl-test.el
+++ test/diff-hl-test.el
@@ -127,7 +127,7 @@
     (should-error (diff-hl-next-hunk) :type 'user-error)))
 
 (diff-hl-deftest diff-hl-indirect-buffer-move-async ()
-  (skip-when (< emacs-major-version 27)) ;No `main-thread'.
+  (skip-unless (>= emacs-major-version 27)) ;No `main-thread'.
   (diff-hl-test-in-source
    (let ((diff-hl-update-async t))
      (narrow-to-region (point-min) (point-max))
