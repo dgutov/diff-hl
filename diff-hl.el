0574c3247ce50cfce6bf9127ac568684be1e2ada
Dmitry Gutov <dmitry@gutov.dev>
2025-11-10 16:57:10 +0200

    diff-hl--update-buffer: Check for liveness
    Fixes #259

--- diff-hl.el
+++ diff-hl.el
@@ -645,8 +645,9 @@ contents as they are (or would be) after applying the changes in NEW."
     (diff-hl--update)))
 
 (defun diff-hl--update-buffer (buf)
-  (with-current-buffer buf
-    (diff-hl-update)))
+  (when (buffer-live-p buf)
+    (with-current-buffer buf
+      (diff-hl-update))))
 
 (defun diff-hl-with-editor-p (_dir)
   (bound-and-true-p with-editor-mode))
